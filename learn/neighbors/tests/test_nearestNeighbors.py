1addc18740797a5d5ca917692935c2fef055fb34
demon song <787423495@qq.com>
2017-03-14 14:03:21 +0800

    update master

---
+++ learn/neighbors/tests/test_nearestNeighbors.py
@@ -0,0 +1,9 @@
+from sklearn.neighbors import NearestNeighbors
+import numpy as np
+
+
+X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
+nbrs = NearestNeighbors(n_neighbors=2, algorithm='ball_tree').fit(X)
+distances, indices = nbrs.kneighbors(X)
+
+
